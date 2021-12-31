414f80522362032b56af802a638652bb00714768
p51lee <p51lee@kaist.ac.kr>
2021-12-31 16:40:07 +0900

    Simplify hindex.py, exprbyn.py

--- p51lee/w02_pgs_dp_exprbyn.py
+++ p51lee/w02_pgs_dp_exprbyn.py
@@ -3,21 +3,18 @@ def solution(N, number):
     series_of_N = [int(N_string * i) for i in range(1, 9)]
     solution_dict = {i: {series_of_N[i - 1]} for i in range(1, 9)}
 
-    for sol in range(1, 9):
-        if number in solution_dict[sol]:
-            return sol
-        for op1 in solution_dict[sol]:
-            for order in range(1, min(sol, 8 - sol) + 1):
-                for eqn_num in solution_dict[order]:
-                    solution_dict[sol + order].update(
-                        [op1 + eqn_num, abs(op1 - eqn_num), op1 * eqn_num]
+    for ord1 in range(1, 9):
+        if number in solution_dict[ord1]:
+            return ord1
+        for op1 in solution_dict[ord1]:
+            for ord2 in range(1, min(ord1, 8 - ord1) + 1):
+                for op2 in solution_dict[ord2]:
+                    solution_dict[ord1 + ord2].update(
+                        [op1 + op2, abs(op1 - op2), op1 * op2]
                     )
-                    # solution_dict[sol + order].add(op1 + eqn_num)
-                    # solution_dict[sol + order].add(abs(op1 - eqn_num))
-                    # solution_dict[sol + order].add(op1 * eqn_num)
-                    if eqn_num != 0 and op1 % eqn_num == 0:
-                        solution_dict[sol + order].add(op1 / eqn_num)
-                    if op1 != 0 and eqn_num % op1 == 0:
-                        solution_dict[sol + order].add(eqn_num / op1)
+                    if op2 != 0 and op1 % op2 == 0:
+                        solution_dict[ord1 + ord2].add(op1 / op2)
+                    if op1 != 0 and op2 % op1 == 0:
+                        solution_dict[ord1 + ord2].add(op2 / op1)
 
     return -1
