a2bd3ebc096b628476252970e127568440ed30b9
KangUk Lee <p51lee@kaist.ac.kr>
2022-02-03 22:23:28 +0900

    [PGS] 캐쉬 / level 2 / 0.5hr / #34

--- p51lee/w07/kakao_2018/4_cache.py
+++ p51lee/w07/kakao_2018/4_cache.py
@@ -1,3 +1,22 @@
+from collections import deque
+
 def solution(cacheSize, cities):
+    cities_dq = deque(map(lambda s: s.lower(), cities))
+    cities_num = len(cities)
+    cache = deque()
+    cacheAlloc = 0
     answer = 0
+    while cities_dq:
+        curr = cities_dq.popleft()
+        if curr in cache:
+            cache.remove(curr)
+            cache.append(curr)
+            answer += 1
+        else:
+            answer += 5
+            cache.append(curr)
+            if cacheAlloc == cacheSize:
+                cache.popleft()
+            else:
+                cacheAlloc += 1
     return answer
