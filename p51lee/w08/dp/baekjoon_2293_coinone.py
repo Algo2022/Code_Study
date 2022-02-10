37278b0c3e127f9d168904654d063276e5921d29
Kanguk Lee <p51lee@kaist.ac.kr>
2022-02-10 16:43:42 +0900

    [BOJ] 코인원 / Gold 5 / 0.5hr / #37

--- p51lee/w08/dp/baekjoon_2293_coinone.py
+++ p51lee/w08/dp/baekjoon_2293_coinone.py
@@ -1,39 +1,20 @@
 import sys
 from collections import defaultdict
 
-class Case():
-    def __init__(self, n, coins=defaultdict(int), sum=0):
-        self.n = n
-        self.coins = coins
-        self.sum = sum
-
-    def isValid(self):
-        return self.sum == n
-
-    def addCoin(self, new_coin):
-        # print(new_coin)
-        ret = [self]
-        sum_temp = self.sum + new_coin
-        while sum_temp <= self.n:
-            self.coins[new_coin] += 1
-            # print(self.coins[new_coin])
-            ret.append(Case(self.n, self.coins.copy(), sum_temp))
-            sum_temp += new_coin
-        return ret
-
-    def __str__(self):
-        return str(self.coins) + str(self.sum)
-
-flat_map = lambda f, xs: [y for ys in xs for y in f(ys)]
-
 num_coins, n = map(int, sys.stdin.readline().split())
 
 coins = []
 for _ in range(num_coins):
     coins.append(int(sys.stdin.readline()))
 
-cases = [Case(n)]
-for coin in coins:
-    cases = flat_map(lambda c: c.addCoin(coin), cases)
+cases = defaultdict(int)
+coins.sort(reverse=True)
 
-print(len(list(filter(lambda x: x.isValid(), cases))))
+for coin in coins:
+    cases[coin] += 1
+    for num_curr in range(1, n+1):
+        if num_curr > coin:
+            cases[num_curr] += cases[num_curr-coin]
+
+print(cases[n])
+
